e88906d14a38b966f685a48b1f72fb21b5affbd7
ZBOOK\Clark <clarkremon12@gmail.com>
2025-11-03 10:39:38 +0200

    feat: implemented animated splash screen - added package animate do for easy animation - added placeholder for onboarding for navigation

--- lib/feature/splash/splash.dart
+++ lib/feature/splash/splash.dart
@@ -1,11 +1,45 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:tasky_nti/core/constants/app_constants.dart';
+import 'package:tasky_nti/core/theming/app_colors.dart';
+import 'package:tasky_nti/feature/onboarding/onboarding_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
   static const String routeName = '/splash';
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnboardingScreen.routeName);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeInLeft(child: Image.asset(AppConstants.taskSplashIcon)),
+            BounceInUp(
+              delay: Duration(milliseconds: 500),
+              child: Align(
+                alignment: AlignmentGeometry.xy(0.0, 0.02),
+                child: Image.asset(AppConstants.yIcon),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
