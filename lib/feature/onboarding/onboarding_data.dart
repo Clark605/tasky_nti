d09aad29f90935b9f22b5091c8d1206a395953c8
ZBOOK\Clark <clarkremon12@gmail.com>
2025-11-04 11:29:41 +0200

    feat: implement onboarding flow with animated widgets and navigation - added package smooth_page_indicator - updated onboarding images with higher qulity ones - added new grey color in app_colors.dart - added login_screen placeholder

---
+++ lib/feature/onboarding/onboarding_data.dart
@@ -0,0 +1,34 @@
+import 'package:tasky_nti/core/constants/app_constants.dart';
+
+class OnboardingData {
+  final String image;
+  final String title;
+  final String subtitle;
+  OnboardingData({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+  });
+  static List<OnboardingData> dataOnBoarding() {
+    return [
+      OnboardingData(
+        image: AppConstants.onBoardingImage1,
+        title: 'Manage your tasks',
+        subtitle:
+            'You can easily manage all of your daily tasks in DoMe for free',
+      ),
+      OnboardingData(
+        image: AppConstants.onBoardingImage2,
+        title: 'Create daily routine',
+        subtitle:
+            'In Tasky you can create your personalized routine to stay productive',
+      ),
+      OnboardingData(
+        image: AppConstants.onBoardingImage3,
+        title: 'Organize your tasks',
+        subtitle:
+            'You can organize your daily tasks by adding your tasks into separate categories',
+      ),
+    ];
+  }
+}
---
+++ lib/feature/onboarding/widgets/custom_animated_widget.dart
@@ -0,0 +1,34 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+class CustomAnimatedWidget extends StatelessWidget {
+  final int index;
+  final int delay;
+  final Widget child;
+
+  const CustomAnimatedWidget({
+    super.key,
+    required this.index,
+    required this.delay,
+    required this.child,
+  });
+  @override
+  Widget build(BuildContext context) {
+    if (index == 1) {
+      return FadeInDown(
+        delay: Duration(milliseconds: delay),
+        child: child,
+      );
+    } else if (index == 2) {
+      return FadeInUp(
+        delay: Duration(milliseconds: delay),
+        child: child,
+      );
+    } else {
+      return FadeInDown(
+        delay: Duration(milliseconds: delay),
+        child: child,
+      );
+    }
+  }
+}
