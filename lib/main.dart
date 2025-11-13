80386f5b148263a9c63e8234647f6f5478b9ad6c
ZBOOK\Clark <clarkremon12@gmail.com>
2025-11-13 22:44:18 +0200

    feat: integrate Firebase authentication and Firestore support

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tasky_nti/feature/auth/login_screen.dart';
 import 'package:tasky_nti/feature/auth/register_screen.dart';
 import 'package:tasky_nti/feature/home/home_screen.dart';
 import 'package:tasky_nti/feature/onboarding/onboarding_screen.dart';
 import 'package:tasky_nti/feature/splash/splash.dart';
+import 'package:tasky_nti/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
